d54c1e5206afcf155f9e2c808d0594a6940528bd
mqw1 <mqw1@students.waikato.ac.nz>
2025-09-08 17:01:58 +1200

    add the route for getting data from server

--- frontend/src/views/Planner.vue
+++ frontend/src/views/Planner.vue
@@ -59,74 +59,43 @@ export default {
     };
   },
   methods: {
-    generateItinerary() {
-      // dummy data
-      if (this.plan.city === "Queenstown") {
-        this.itinerary = [
-          {
-            time: "09:00",
-            activity: "Skyline Gondola & Luge",
-            location: "Brecon St, Queenstown",
-            note: "Great views to start your trip!",
-          },
-          {
-            time: "13:00",
-            activity: "Lunch at Fergburger",
-            location: "Queenstown CBD",
-            note: "Famous burger spot 🍔",
-          },
-          {
-            time: "15:00",
-            activity: "Kawarau Bridge Bungy",
-            location: "State Hwy 6, Gibbston",
-            note: "Thrilling adventure jump!",
-          },
-          {
-            time: "19:00",
-            activity: "Dinner by the lake",
-            location: "Lake Wakatipu",
-            note: "Relax with sunset view 🌅",
-          },
-        ];
-      } else if (this.plan.city === "Auckland") {
-        this.itinerary = [
-          {
-            time: "10:00",
-            activity: "Sky Tower Visit",
-            location: "Victoria St W, Auckland",
-            note: "Panoramic city view",
-          },
-          {
-            time: "12:30",
-            activity: "Lunch at Viaduct Harbour",
-            location: "Viaduct, Auckland",
-            note: "Seafood and harbor vibes 🐟",
-          },
-          {
-            time: "15:00",
-            activity: "Auckland War Memorial Museum",
-            location: "Auckland Domain",
-            note: "Indoor option if it rains",
-          },
-          {
-            time: "18:00",
-            activity: "Evening stroll at Mission Bay",
-            location: "Mission Bay Beach",
-            note: "Perfect place to relax",
-          },
-        ];
-      } else {
-        this.itinerary = [
-          {
-            time: "09:00",
-            activity: "City tour",
-            location: this.plan.city,
-            note: "Explore the highlights",
-          },
-        ];
-      }
-    },
-  },
+  async generateItinerary() {
+    // Dummy data dulu
+    let itineraryData = [];
+
+    if (this.plan.city === "Queenstown") {
+      itineraryData = [
+        { time: "09:00", activity: "Skyline Gondola & Luge", location: "Brecon St, Queenstown", note: "Great views to start your trip!" },
+        { time: "13:00", activity: "Lunch at Fergburger", location: "Queenstown CBD", note: "Famous burger spot 🍔" },
+        { time: "15:00", activity: "Kawarau Bridge Bungy", location: "State Hwy 6, Gibbston", note: "Thrilling adventure jump!" },
+        { time: "19:00", activity: "Dinner by the lake", location: "Lake Wakatipu", note: "Relax with sunset view 🌅" },
+      ];
+    } else if (this.plan.city === "Auckland") {
+      itineraryData = [
+        { time: "10:00", activity: "Sky Tower Visit", location: "Victoria St W, Auckland", note: "Panoramic city view" },
+        { time: "12:30", activity: "Lunch at Viaduct Harbour", location: "Viaduct, Auckland", note: "Seafood and harbor vibes 🐟" },
+        { time: "15:00", activity: "Auckland War Memorial Museum", location: "Auckland Domain", note: "Indoor option if it rains" },
+        { time: "18:00", activity: "Evening stroll at Mission Bay", location: "Mission Bay Beach", note: "Perfect place to relax" },
+      ];
+    } else {
+      itineraryData = [
+        { time: "09:00", activity: "City tour", location: this.plan.city, note: "Explore the highlights" },
+      ];
+    }
+
+    this.itinerary = itineraryData;
+
+    // Store in the server
+    try {
+      const userId = localStorage.getItem('userId');
+      await itineraryAPI.create({ userId, city: this.plan.city, date: this.plan.date, itinerary: itineraryData });
+      console.log('Itinerary saved to server');
+    } catch (err) {
+      console.error('Failed to save itinerary', err);
+    }
+  }
+}
+
 };
 </script>
 
